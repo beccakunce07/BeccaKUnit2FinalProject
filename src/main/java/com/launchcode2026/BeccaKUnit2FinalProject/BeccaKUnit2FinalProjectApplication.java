58e4869ecf1b344fd38e8256cb6a3d4887611b3e
beccakunce <rebeccakunce@gmail.com>
2026-08-26 19:41:13 -0500

    first commit

---
+++ src/main/java/com/launchcode2026/BeccaKUnit2FinalProject/BeccaKUnit2FinalProjectApplication.java
@@ -0,0 +1,13 @@
+package com.launchcode2026.BeccaKUnit2FinalProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeccaKUnit2FinalProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeccaKUnit2FinalProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/launchcode2026/BeccaKUnit2FinalProject/BeccaKUnit2FinalProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.launchcode2026.BeccaKUnit2FinalProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeccaKUnit2FinalProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
