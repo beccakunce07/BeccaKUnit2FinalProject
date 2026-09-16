1156fc766ce403989b345dd3a5b1b8e88721a0fc
beccakunce <rebeccakunce@gmail.com>
2026-09-16 00:50:29 -0500

    changed the parameters for LoveBombRepository BK

--- src/main/java/com/launchcode2026/BeccaKUnit2FinalProject/repositories/LoveBombRepository.java
+++ src/main/java/com/launchcode2026/BeccaKUnit2FinalProject/repositories/LoveBombRepository.java
@@ -1,9 +1,9 @@
 package com.launchcode2026.BeccaKUnit2FinalProject.repositories;
 
-import com.launchcode2026.BeccaKUnit2FinalProject.models.UserInfo;
+import com.launchcode2026.BeccaKUnit2FinalProject.models.LoveBomb;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface LoveBombRepository extends JpaRepository <UserInfo, Long> {
+public interface LoveBombRepository extends JpaRepository <LoveBomb, Long> {
 }
